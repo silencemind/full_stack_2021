64ef7f5b81b377ed217d43cafd22a1255ecde497
silencemind <silencemind55@gmail.com>
2021-05-06 01:49:23 +0500

    new

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react'
 
 const Hello = (props) => {
@@ -6,14 +5,28 @@ const Hello = (props) => {
 }
 
 const App = () => {
-
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-  <div>
- 
-    <Hello name='Abid' />
-    <Hello name='Khan' />
-  </div>
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
   )
 }
 

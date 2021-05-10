445686b737ebd1aaacfdb207faa58397b5e56f36
silencemind <silencemind55@gmail.com>
2021-05-11 00:57:06 +0500

    Update node_name_of_file.js

--- part1/src/node_name_of_file.js
+++ part1/src/node_name_of_file.js
@@ -47,3 +47,25 @@ t4 = [1,2,3,4,5,6,7,8]
 const [first,second,third, ... rest] =t4
 console.log(first,second,third)
 console.log(rest)
+
+
+const object1 = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+  }
+  
+  const object2 = {
+    name: 'Full Stack web application development',
+    level: 'intermediate studies',
+    size: 5,
+  }
+  
+  const object3 = {
+    name: {
+      first: 'Dan',
+      last: 'Abramov',
+    },
+    grades: [2, 3, 5, 3],
+    department: 'Stanford University',
+  }

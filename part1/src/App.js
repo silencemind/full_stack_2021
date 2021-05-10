c245e66588ead97f6039d3bbff3b81f197051375
silencemind <silencemind55@gmail.com>
2021-05-10 23:45:15 +0500

    Update App.js

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,7 +6,7 @@ const Header = (props) => {
   console.log(props);
   return (
         <div>
-          
+          <h1>{props.course}</h1>
         </div>
   )
 
@@ -14,24 +14,41 @@ const Header = (props) => {
 
 
 const Content = (props) =>     {
+
+  return (
+    <div>
+      <h2>{props.part1}: {props.exercises1}</h2>
+      <h2>{props.part2}: {props.exercises2}</h2>
+      <h2>{props.part3}: {props.exercises3}</h2>
+    </div>
+  )
+ 
+}
+
+
+const Total = (props) => 
+{
+  return (
+    <div>
+
+      </div>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-}
-
-
-const Total = (props) => 
-{
-  const total = ''
-}
-const App = () => {
-
-  return (
+  return (  
     <div>
-      <Header course={Content} />
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total>
+
+        </Total>
     </div>
   )
 }

97cd9fdd28592ba9d4c2f0b5c548732801ade162
silencemind <silencemind55@gmail.com>
2021-05-12 23:43:06 +0500

    Update App.js

--- part1/src/App.js
+++ part1/src/App.js
@@ -14,12 +14,12 @@ const Header = (props) => {
 
 
 const Content = (props) =>     {
-
+console.log(props)
   return (
     <div>
-      <h2>{props.part1}: {props.exercises1}</h2>
+      {/* <h2>{props.part1}: {props.exercises1}</h2>
       <h2>{props.part2}: {props.exercises2}</h2>
-      <h2>{props.part3}: {props.exercises3}</h2>
+      <h2>{props.part3}: {props.exercises3}</h2> */}
     </div>
   )
  
@@ -31,32 +31,52 @@ const Total = (props) =>
   return (
     <div>
       <h3>
-        Total Exercises: {props.total}
+        {/* Total Exercises: {props.total} */}
       </h3>
       </div>
   )
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  // const part1 = {
+  //   name: 'Fundamentals of React',
+  //   exercises: 10
+  // }
+  // const part2 = {
+  //   name: 'Using props to pass data',
+  //   exercises: 7
+  // }
+  // const part3 = {
+  //   name: 'State of a component',
+  //   exercises: 14
+  // }
   return (  
     <div>
       <Header course={course} />
-      <Content part1={part1['name']} exercises1={part1['exercises']} part2={part2['name']} exercises2={part2['exercises']} part3={part3['name']} exercises3={part3['exercises']} />
-      <Total total= {part1['exercises']+part2['exercises']+part3['exercises']}>
+      
+  
+
+  
+
+      {/* <Content part1={part1['name']} exercises1={part1['exercises']} part2={part2['name']} exercises2={part2['exercises']} part3={part3['name']} exercises3={part3['exercises']} /> */}
+      {/* <Total total= {part1['exercises']+part2['exercises']+part3['exercises']}> */}
         
-        </Total>
+        {/* </Total> */}
     </div>
   )
 }
